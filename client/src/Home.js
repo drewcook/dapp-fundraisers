5157272541b1136d2d2fde1f0993526accb3be58
Drew Cook <drewcook29@gmail.com>
2022-01-02 16:56:22 -0700

    chore: Home page content updates

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,100 +1,52 @@
-import { CircularProgress, Grid, Typography } from '@mui/material'
-import React, { useEffect, useState } from 'react'
-import FundriaserCard from './FundriaserCard'
+import { Box, Container, Typography } from '@mui/material'
+import React from 'react'
+
+const styles = {
+	headlines: {
+		textAlign: 'center',
+		marginBottom: 4,
+	},
+	content: {
+		textAlign: 'center',
+	},
+}
 
 const Home = props => {
 	const { appData } = props
-	const [fundraisers, setFundraisers] = useState([])
-	const [totalCount, setTotalCount] = useState(null)
-	const [loading, setLoading] = useState(true)
-	const [errorMsg, setErrorMsg] = useState(null)
 
-	/* eslint-disable react-hooks/exhaustive-deps */
-	useEffect(() => {
-		init()
-	}, [])
-
-	useEffect(() => {
-		init()
-	}, [appData])
-
-	useEffect(() => {
-		displayContent()
-	}, [loading, errorMsg, fundraisers])
-	/* eslint-enable react-hooks/exhaustive-deps */
-
-	const init = async () => {
-		try {
-			if (appData.factory) fetchFundraisers()
-		} catch (err) {
-			console.error('Init Error:', err.message)
-		}
-	}
-
-	const fetchFundraisers = async () => {
-		setLoading(true)
-		setErrorMsg(null)
-		try {
-			const newFunds = await appData.factory.methods.fundraisers(10, 0).call()
-			const newCount = await appData.factory.methods.fundraisersCount().call()
-			setFundraisers(newFunds)
-			setTotalCount(newCount)
-			setLoading(false)
-		} catch (err) {
-			console.error(err.message)
-			setLoading(false)
-			setErrorMsg('An error occurred while fetching fundraisers. Please check console.')
-		}
-	}
-
-	const displayContent = () => {
-		if (loading)
-			return (
-				<Grid item xs={12} sx={{ textAlign: 'center' }}>
-					<CircularProgress size={30} sx={{ marginX: 'auto', marginY: 4 }} />
-				</Grid>
-			)
-
-		if (errorMsg)
-			return (
-				<Grid item xs={12}>
-					<Typography gutterBottom color="error" sx={{ textAlign: 'center' }}>
-						{errorMsg}
-					</Typography>
-				</Grid>
-			)
-
-		if (fundraisers.length > 0)
-			return fundraisers.map((fund, idx) => (
-				<Grid item xs={12} sm={6} lg={4} key={idx}>
-					<FundriaserCard fundraiser={fund} appData={appData} onDonate={fetchFundraisers} />
-				</Grid>
-			))
-
-		return (
-			<Grid item xs={12}>
-				<Typography gutterBottom variant="overline">
-					No fundraisers created yet.
-				</Typography>
-			</Grid>
-		)
-	}
+	console.log('home', appData)
 
 	return (
 		<>
-			<Typography gutterBottom variant="h2" sx={{ textAlign: 'center' }}>
-				Home
-			</Typography>
-			<Grid container spacing={2}>
-				{totalCount && (
-					<Grid item xs={12}>
-						<Typography gutterBottom sx={{ textAlign: 'center' }}>
-							{totalCount} Total Fundraisers to donate to
-						</Typography>
-					</Grid>
-				)}
-				{displayContent()}
-			</Grid>
+			<Box sx={styles.headlines}>
+				<Typography gutterBottom variant="h2">
+					Welcome to Fundraisers DApp
+				</Typography>
+				<img src="/logo.png" title="Fundraisers" alt="Fundraisers" width="200" height="200" />
+				<Typography gutterBottom variant="h4" component="h3">
+					Create positive change in the world!
+				</Typography>
+			</Box>
+			<Container maxWidth="sm" sx={styles.content}>
+				<Typography gutterBottom>
+					This decentralized application is a space for creating fundraisers and making donations to
+					them, all on the Ethereum blockchain. You should already have a wallet connected to this
+					site, and therefore, you will be able to participate in both creating and donating to
+					fundraisers.
+				</Typography>
+				<Typography gutterBottom>
+					As a creator of a fundraiser, you will be able to manage the funds of that fundraiser.
+					Other users can make donations in the form of Ether to your fund, and you will act as a
+					custodian for these funds. You may withdraw the funds out to be donated directly into a
+					beneficiary account. All of this is handled via smart contracts deployed on the Ethereum
+					Rinkeby testnet.{' '}
+					<strong>Ensure your wallet is connected and on the Rinkeby testnet.</strong>
+				</Typography>
+				<Typography gutterBottom>
+					Check out some of the causes created by other users on the <strong>Explore</strong> page,
+					or set up a new fundraiser by clicking the <strong>Create</strong> link.
+				</Typography>
+			</Container>
 		</>
 	)
 }
